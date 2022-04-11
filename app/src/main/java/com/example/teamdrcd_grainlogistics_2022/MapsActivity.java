dbf8e2fff04e3b351b0a4e58c7557d97b7c2ab56
CaseyThePigeon <98485716+CaseyThePigeon@users.noreply.github.com>
2022-04-11 14:04:57 -0500

    Slide up function with buttons in slide up

--- app/src/main/java/com/example/teamdrcd_grainlogistics_2022/MapsActivity.java
+++ app/src/main/java/com/example/teamdrcd_grainlogistics_2022/MapsActivity.java
@@ -1,26 +1,16 @@
 package com.example.teamdrcd_grainlogistics_2022;
 
-import static java.lang.Thread.sleep;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.LocationRequest;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -30,18 +20,11 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.teamdrcd_grainlogistics_2022.databinding.ActivityMapsBinding;
-import com.google.android.gms.tasks.CancellationToken;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.OnTokenCanceledListener;
 
-@SuppressWarnings("deprecation")
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private FusedLocationProviderClient fusedLocationClient;
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-    //private sLocationRequest lr = new LocationRequest();
-    //lr.setPriority(100);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +38,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                new String[] {"Test Button", "Second Button"}));
     }
 
     /**
@@ -70,7 +53,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
@@ -78,137 +61,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(quadcities).title("Marker in Bettendorf"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(quadcities));
 
+        // Add a marker in Sydney and move the camera
         LatLng Tractor1 = new LatLng(41.557579, -90.495911);
-        //mMap.addMarker(new MarkerOptions().position(Tractor1).title("Tractor #1")
+        mMap.addMarker(new MarkerOptions().position(Tractor1).title("Tractor #1")
                 // below line is use to add custom marker on our map.
-        //        .icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_baseline_agriculture_24)));
+                .icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_baseline_agriculture_24)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Tractor1));
-
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            //return;
-        }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        CancellationToken ct = new CancellationToken() {
-            @NonNull
-            @Override
-            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
-                return null;
-            }
-
-            @Override
-            public boolean isCancellationRequested() {
-                return false;
-            }
-        };
-        googleMap.setMyLocationEnabled(true);
-        //Location location = googleMap.getMyLocation();
-        fusedLocationClient.getCurrentLocation(100, ct)
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                            mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("You")
-                                    // below line is use to add custom marker on our map.
-                                    .icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_baseline_agriculture_24)));
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
-
-                        } else {
-                            mMap.addMarker(new MarkerOptions().position(new LatLng(41.542670, -90.477620)).title("You")
-                                    // below line is use to add custom marker on our map.
-                                    .icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_baseline_agriculture_24)));
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(41.542670, -90.477620)));
-
-                        }
-                    }
-                });
     }
-
-    private void Update()
-    {
-        CancellationToken ct = new CancellationToken() {
-            @NonNull
-            @Override
-            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
-                return null;
-            }
-
-            @Override
-            public boolean isCancellationRequested() {
-                return false;
-            }
-        };
-
-        //while (true)
-        {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-
-            fusedLocationClient.getCurrentLocation(100, ct)
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            // Got last known location. In some rare situations this can be null.
-                            if (location != null) {
-                                // Logic to handle location object
-                                mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("You")
-                                        // below line is use to add custom marker on our map.
-                                        .icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_baseline_agriculture_24)));
-                                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
-
-                            } else {
-                                mMap.addMarker(new MarkerOptions().position(new LatLng(41.542670, -90.477620)).title("You")
-                                        // below line is use to add custom marker on our map.
-                                        .icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_baseline_agriculture_24)));
-                                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(41.542670, -90.477620)));
-
-                            }
-                        }
-                    });
-            // Add a marker in Sydney and move the camera
-            try {
-                sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-
 
     private BitmapDescriptor BitmapFromVector(Context context, int vectorResId) {
         // below line is use to generate a drawable.
         Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
 
         // below line is use to set bounds to our vector drawable.
-        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        vectorDrawable.setBounds(20, 20, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
 
         // below line is use to create a bitmap for our
         // drawable which we have added.
